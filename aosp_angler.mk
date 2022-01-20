e751825784a213135e9e745a8e842aab4724fee3
kongwoojin <kongwoojin03@gmail.com>
2022-01-20 17:44:54 +0900

    angler: Fix build error
    Change-Id: I85d36373c615129d3e1afc9dbbc873cf06600c3e

--- aosp_angler.mk
+++ aosp_angler.mk
@@ -24,10 +24,9 @@ PRODUCT_COPY_FILES := device/huawei/angler/configs/apns-full-conf.xml:system/etc
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common PixelExperience stuff
-TARGET_BOOT_ANIMATION_RES := 1440
-TARGET_GAPPS_ARCH := arm64
-$(call inherit-product, $(TOPDIR)vendor/aosp/config/common_full_phone.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 PRODUCT_NAME := aosp_angler
 PRODUCT_DEVICE := angler
