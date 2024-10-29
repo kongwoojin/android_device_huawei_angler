d4253fa39fbdc5dd007bd5a68b84e887200a6b43
WooJin Kong <kongwoojin03@gmail.com>
2024-10-29 12:21:54 +0900

    angler: Disable serial console
    Change-Id: I358d96a9b06f2e7b8a1ed0e6d7f4ab3cffda0924

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,7 @@ BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=angler androidboot.console=ttyHSL0 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-3 no_console_suspend swiotlb=2048
+BOARD_KERNEL_CMDLINE := androidboot.hardware=angler msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-3 swiotlb=2048
 BOARD_KERNEL_CMDLINE += skip_initramfs rootwait ro init=/init root=/dev/dm-0
 BOARD_KERNEL_CMDLINE += dm=\"system none ro,0 1 android-verity /dev/mmcblk0p43\"
 BOARD_KERNEL_CMDLINE += loop.max_part=7 androidboot.selinux=permissive
